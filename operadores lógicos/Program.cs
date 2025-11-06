a3073df481540e943f1366ee4c3c9cae06a18c95
FELIPE MIRANDA MARTINS LUIZ <felipe.mmluiz@senacsp.edu.br>
2025-11-06 14:58:15 -0300

    enviando atualização

---
+++ operadores lógicos/Program.cs
@@ -0,0 +1,12 @@
+﻿bool x = true;
+bool y = true;
+
+bool and = x && y; // e lógico
+bool or  = x || y; // ou lógico
+bool not  = !x; //não lógico
+
+//exibindo resultados dos operadores lógicos
+Console.WriteLine("\nOperadores lógicos:"); 
+Console.WriteLine($"x && y: {and}"); // e lógico
+Console.WriteLine($"x || y: {or}"); // ou lógico
+Console.WriteLine($"!x: {not}"); //não lógico
---
+++ operadores relacionais/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Mail;
+
+int a = 10;
+int b = 5;
+
+//operadores relacionais
+bool maiorQue = a > b;
+bool menorQue = a < b;
+bool igual = a == b;
+bool diferente = a != b;
+bool mairoOuIgual = a >= b;
+bool menorOuIgual = a <= b;
+
+//exibindo resultados dos operadore relacionais
+Console.WriteLine("\noperadores relacionais:");
+Console.WriteLine($"{a} > {b}: {maiorQue} ");
+Console.WriteLine($"{a} > {b}: {menorQue}");
+Console.WriteLine($"{a} > {b}: {igual} ");
+Console.WriteLine($"{a} > {b}: {diferente}");
+Console.WriteLine($"{a} > {b}: {mairoOuIgual} ");
+Console.WriteLine($"{a} > {b}: {menorOuIgual}");
