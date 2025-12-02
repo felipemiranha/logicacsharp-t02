a1ca6ee9c76d46b6a0a38a96ddf794cee2396c07
FELIPE MIRANDA MARTINS LUIZ <felipe.mmluiz@senacsp.edu.br>
2025-12-02 16:56:06 -0300

    27/07

---
+++ calsses/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Text;
+
+using System.Threading.Tasks;
+
+namespace logica13
+
+{
+
+    //Definiçao da classe carro
+
+    internal class carro
+
+    {
+
+        //Propriedade pública: Pode ser acessada e modificada fora da classe
+
+        public string Marca { get; set; } //publica a propriedade Marca
+
+        //Propriedade privada: não pode ser acessada ou modificada fora da classe
+
+        private string Modelo { get; set; } //Privada, acessível apenas dentro da classe
+
+        //Propriedade privada com um campo privado e um método getter e setter personalizados
+
+        private int _ano; //Campo privado
+
+        public int Ano
+        {
+            get { return _ano; } //Getter, para acessar o valor do campo privadoss
+            set
+            {
+                if (value > 1900 && value <= DateTime.Now.Year) //Verificação para garantir um valor válido
+                {
+                    _ano = value;
+                }
+                else
+                {
+                    Console.WriteLine("Ano inválido");
+                }
+            }
+        }
+    }
+
+}
+
